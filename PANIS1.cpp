2271576f37d7e21a991ea22bf4d9f1ae41e9af96
Gelo <mapanis@gbox.adnu.edu.ph>
2025-08-15 14:23:33 +0800

    Create PANIS1.cpp

---
+++ PANIS1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Assign the values to variables a, b, and c
+    string a = "I";
+    string b = "Love";
+    string c = "You";
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;  // Input a number, for example, 3
+    cout << a << " " << endl;
+    
+    
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << b << " " << endl;
+    
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << c << " " << endl;
+    
+    cout << "The word is: " << a << " " << b << " " << c << " " << endl;
+    
+    return 0;
+    
+}
