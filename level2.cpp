96a706e68ff32414c928c010ef6a631bbee2cd25
Gelo <mapanis@gbox.adnu.edu.ph>
2025-08-22 14:43:37 +0800

    Create level2.cpp

---
+++ level2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num1 = 169, num2 = 286, average;
+    average = (num1 + num2)/2;
+ 
+    
+    if(num1 > average){
+    cout << "You did better than average";
+    
+    }
+    
+    else
+    cout << "You did worse than average";
+    
+    return 0;
+}
